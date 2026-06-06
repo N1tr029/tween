de97737743ad917b0650c8aba0e5c1b4706896b4
Kavi Gandham <kavi.gandham37@gmail.com>
2026-06-06 11:53:03 -0400

    Initial Commit

---
+++ TweenApp/TweenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TweenAppApp.swift
+//  TweenApp
+//
+//  Created by Kavi Gandham on 6/6/26.
+//
+
+import SwiftUI
+
+@main
+struct TweenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
